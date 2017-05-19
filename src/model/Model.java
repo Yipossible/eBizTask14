d97bfbecd590451ae2eb845f4830c21ad89a46a1
Yipossible <yi.wang.carnegie.mellon@gmail.com>
2017-05-18 20:01:31 -0400

    Model updated

--- src/model/Model.java
+++ src/model/Model.java
@@ -8,6 +8,10 @@ import org.genericdao.DAOException;
 
 public class Model {
 	private AllDataDAO allDataDAO;
+	private DiseaseDAO diseaseDAO;
+	private GroceryStoreDAO groceryStoreDAO;
+	private InsuranceCompanyDAO insuranceCompanyDAO;
+	private InsuranceHealthDAO insuranceHealthDAO;
 
 	
     private boolean requireSSL;
@@ -20,6 +24,11 @@ public class Model {
             
             ConnectionPool pool = new ConnectionPool(jdbcDriver, jdbcURL);
             allDataDAO  = new AllDataDAO("allData", pool);
+            diseaseDAO = new DiseaseDAO("disease_id", pool);
+            groceryStoreDAO = new GroceryStoreDAO("grocery_store", pool);
+            insuranceCompanyDAO = new InsuranceCompanyDAO("insurance_company", pool);
+            insuranceHealthDAO = new InsuranceHealthDAO("insurance_health", pool);
+            
 
            
         } catch (DAOException e) {
@@ -30,6 +39,46 @@ public class Model {
 
 
 
+
+	public AllDataDAO getAllDataDAO() {
+		return allDataDAO;
+	}
+
+	public void setAllDataDAO(AllDataDAO allDataDAO) {
+		this.allDataDAO = allDataDAO;
+	}
+
+	public DiseaseDAO getDiseaseDAO() {
+		return diseaseDAO;
+	}
+	
+	public void setDiseaseDAO(DiseaseDAO diseaseDAO) {
+		this.diseaseDAO = diseaseDAO;
+	}
+
+	public GroceryStoreDAO getGroceryStoreDAO() {
+		return groceryStoreDAO;
+	}
+
+	public void setGroceryStoreDAO(GroceryStoreDAO groceryStoreDAO) {
+		this.groceryStoreDAO = groceryStoreDAO;
+	}
+
+	public InsuranceCompanyDAO getInsuranceCompanyDAO() {
+		return insuranceCompanyDAO;
+	}
+
+	public void setInsuranceCompanyDAO(InsuranceCompanyDAO insuranceCompanyDAO) {
+		this.insuranceCompanyDAO = insuranceCompanyDAO;
+	}
+
+	public InsuranceHealthDAO getInsuranceHealthDAO() {
+		return insuranceHealthDAO;
+	}
+
+	public void setInsuranceHealthDAO(InsuranceHealthDAO insuranceHealthDAO) {
+		this.insuranceHealthDAO = insuranceHealthDAO;
+	}
 
 	public void setRequireSSL(boolean requireSSL) {
 		this.requireSSL = requireSSL;
