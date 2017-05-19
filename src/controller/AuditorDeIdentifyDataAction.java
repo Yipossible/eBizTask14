1ced51b78b6788199a23701b999acefa5ae2ac71
Yipossible <yi.wang.carnegie.mellon@gmail.com>
2017-05-18 20:07:36 -0400

    rename actions

---
+++ src/controller/AuditorDeIdentifyDataAction.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AuditorDeIdentifyDataAction extends Action {
+
+	public String getName () {return "auditordeIdentification.do";}
+	
+	public String perform (HttpServletRequest request) {
+		
+		
+		//
+		
+		
+		
+		return "auditordeIdentification.jsp";
+	}
+}
